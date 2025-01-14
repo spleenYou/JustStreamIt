04a6da22383b6ceb7f45887e95258f80b9a40e20
Anthony YOU <spleen85@proton.me>
2025-01-14 11:12:01 +0100

    JS add comment in pictureMovie.js and clean code

--- js/pictureMovie.js
+++ js/pictureMovie.js
@@ -1,3 +1,4 @@
+// Ajout du code HTML pour une image de film
 function addPictureMovie() {
     let pictureMovie = document.createElement("div")
     pictureMovie.appendChild(document.createElement("img"))
@@ -11,14 +12,15 @@ function addPictureMovie() {
     return pictureMovie
 }
 
+// Ajout au conteneur du titre et des vignettes de films
 function addToContainer(container, categoryNumber) {
     category = categoryTab.find((cat) => cat.name == categoriesDisplay[categoryNumber])
     container.appendChild(document.createElement("h1"))
     container.children[0].innerHTML = category.title
     container.appendChild(addDivPicturesMovie())
-    return title
 }
 
+// Ajout de la div contenant les vignettes de films
 function addDivPicturesMovie() {
     let div = document.createElement("div")
     div.classList.add("picturesMovie")
@@ -28,28 +30,37 @@ function addDivPicturesMovie() {
     return div
 }
 
+// Remplissage des conteneurs
 function fillContainers() {
+    // Recherche des conteneurs
     let containers = document.getElementsByClassName("container")
     for (let i = 0; i < containers.length; i++) {
+        // S'il s'agit d'un conteneur qui n'est pas la catégorie "other", on remplit le conteneur
         if (i != (containers.length - 1)) {
             addToContainer(containers[i], i)
             findNumberPages(categoriesDisplay[i])
         } else {
+            // Sinon on met le code HTML sans remplissage
             containers[i].appendChild(addDivPicturesMovie())
         }
+        // Création du bouton "Voir plus"/"Voir moins"
         let button = document.createElement("button")
         button.classList.add("buttonMoreLess")
         button.innerHTML = "Voir plus"
+        // Ajout de l'event sur le bouton
         button.addEventListener("click", (e) => {
             let viewMode = "less"
             if (e.target.innerHTML == "Voir plus") {
                 viewMode = "more"
             }
+            // Mise à jour de la vue selon le format d'écran
             updateViewPicturesMovie(e.target.parentNode.children[1], viewMode)
         })
         containers[i].appendChild(button)
+        // Mise à jour de la vue selon le format d'écran
         updateViewPicturesMovie(containers[i].children[1], "less")
     }
+    // Gestion de l'affichage des img des films si elles n'existent pas
     let imgs = document.getElementsByTagName("img")
     for (let i = 0; i < imgs.length; i++) {
         imgs[i].addEventListener("error", (e) => {
@@ -58,6 +69,7 @@ function fillContainers() {
     }
 }
 
+// Mise à jour de la vue selon le format d'écran et du texte du bouton
 function updateViewPicturesMovie(container, viewMode) {
     let viewNumber = thumbnailNumber.default
     container.nextSibling.innerHTML = "Voir moins"
@@ -70,6 +82,7 @@ function updateViewPicturesMovie(container, viewMode) {
             viewNumber = thumbnailNumber.smartphone
         }
     }
+    // Mise à jour du display selon e nombre à afficher
     for (let i = 0; i < container.children.length; i++) {
         container.children[i].style.display = i < viewNumber
             ? "block"
@@ -77,6 +90,7 @@ function updateViewPicturesMovie(container, viewMode) {
     }
 }
 
+// En cas de modification de l'écran, on met à jour la vue
 window.addEventListener("resize", () => {
     let containers = document.getElementsByClassName("container")
     for (let i = 0; i < containers.length; i++) {
